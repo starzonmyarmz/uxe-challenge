16cf9aa237837ec183c0b87177ffd7ec6b93f450
Daniel Marino <starzonmyarmz@gmail.com>
2025-02-14 09:26:57 -0500

    Budgets and Categories!

--- src/components/Budget/index.tsx
+++ src/components/Budget/index.tsx
@@ -1,11 +1,214 @@
+import { ChangeEvent, useState } from "react"
+import { Button } from "../../elements/Button"
+import { Input } from "../../elements/Input"
 import { Pane } from "../../elements/Pane"
+import { VisuallyHidden } from "../../utils/VisuallyHidden"
+import {
+  Add,
+  DiningOut,
+  Food,
+  HealthFitness,
+  Housing,
+  Transportation,
+  Clothing,
+} from "../../icons"
 
 import "./Budget.scss"
 
+// Lets format our numbers using a decorator so
+// that we can use numbers in our data in case we
+// want to do some math later on!
+const numFormat = new Intl.NumberFormat("default", {
+  style: "currency",
+  currency: "USD",
+  maximumFractionDigits: 0,
+})
+
+const summary = [
+  {
+    name: "Monthly Income",
+    amount: 4000,
+  },
+  {
+    name: "Allocated",
+    amount: 2500,
+  },
+  {
+    name: "Remaining",
+    amount: 1500,
+  },
+]
+
+const categories = [
+  {
+    name: "Transportation",
+    amount: 400,
+    icon: <Transportation aria-hidden="true" />,
+  },
+  {
+    name: "Housing",
+    amount: 1500,
+    icon: <Housing aria-hidden="true" />,
+  },
+  {
+    name: "Food",
+    amount: 500,
+    icon: <Food aria-hidden="true" />,
+  },
+  {
+    name: "Dining Out",
+    amount: 500,
+    icon: <DiningOut aria-hidden="true" />,
+  },
+  {
+    name: "Health and Fitness",
+    amount: 200,
+    icon: <HealthFitness aria-hidden="true" />,
+  },
+]
+
+const categoryOptions = [
+  {
+    name: "Transportation",
+    icon: <Transportation aria-hidden="true" />,
+  },
+  {
+    name: "Housing",
+    icon: <Housing aria-hidden="true" />,
+  },
+  {
+    name: "Food",
+    icon: <Food aria-hidden="true" />,
+  },
+  {
+    name: "Dining Out",
+    icon: <DiningOut aria-hidden="true" />,
+  },
+  {
+    name: "Health and Fitness",
+    icon: <HealthFitness aria-hidden="true" />,
+  },
+  {
+    name: "Clothing",
+    icon: <Clothing aria-hidden="true" />,
+  },
+]
+
 export const Budget = () => {
+  // Toggle between budget and new category state
+  const [budgetState, setBudgetState] = useState("budget")
+
+  // States for New Category form
+  const [name, setName] = useState("")
+  const [category, setCategory] = useState("")
+  const [limit, setLimit] = useState("")
+
   return (
-    <Pane size="md" title="Budget">
-      <>{/* "budget categories" UI goes here */}</>
-    </Pane>
+    <>
+      {/* Looking at the Budget view */}
+      {budgetState === "budget" && (
+        <Pane size="md" title="Budget">
+          <ul className="budget-summary">
+            {summary.map(({ name, amount }, index) => (
+              <li key={index}>
+                {name}
+                <h2 className="mt-05">{numFormat.format(amount)}</h2>
+              </li>
+            ))}
+          </ul>
+
+          <h2 className="categories-title">Categories</h2>
+          <ol className="slats-list">
+            {categories.map(({ amount, icon, name }, index) => (
+              <li key={index} className="slat">
+                <div className="slat--icon">{icon}</div>
+                <div className="slat--details flex flex-between flex-baseline">
+                  <h3 className="slat--title">{name}</h3>
+                  {numFormat.format(amount)}
+                </div>
+              </li>
+            ))}
+          </ol>
+
+          <Button
+            variant="outline"
+            onClick={() => setBudgetState("new")}
+            className="w-100%"
+          >
+            <Add /> New category
+          </Button>
+        </Pane>
+      )}
+
+      {/* Looking at the New Category view */}
+      {budgetState === "new" && (
+        <Pane size="md" title="New category">
+          <div className="category-form">
+            <div className="category-form--row">
+              <label htmlFor="name">Category name:</label>
+              <Input
+                id="name"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setName(e.target.value)
+                }
+                type="text"
+                value={name}
+              />
+            </div>
+
+            <div className="category-form--row">
+              <div className="radio-choices">
+                <legend>Icon:</legend>
+                <ul>
+                  {categoryOptions.map(({ name, icon }, index) => (
+                    <li key={index}>
+                      <label className="radio-choice">
+                        <VisuallyHidden>{name}</VisuallyHidden>
+                        {icon}
+                        <input
+                          type="radio"
+                          name="icon"
+                          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            setCategory(e.target.value)
+                          }
+                          value={name}
+                          checked={name === category}
+                        />
+                      </label>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+
+            <div className="category-form--row">
+              <label htmlFor="limit">Limit:</label>
+              <Input
+                id="limit"
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  setLimit(e.target.value)
+                }
+                type="text"
+                value={limit}
+              />
+            </div>
+
+            <div className="category-form--actions">
+              <Button
+                text="Cancel"
+                variant="outline"
+                onClick={() => setBudgetState("budget")}
+                className="w-100%"
+              />
+              <Button
+                text="Add new category"
+                onClick={() => setBudgetState("budget")}
+                className="w-100%"
+              />
+            </div>
+          </div>
+        </Pane>
+      )}
+    </>
   )
 }
